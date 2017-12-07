3fefa59c99c2a9a213535928fba6ae6e897080e0
Michael Glaser <m.glaser82@gmail.com>
2017-12-06 16:18:53 -0800

    Updates bottle_song_lyrics.rb to add lyrics to the lyrics string for 99 bottles of beer. Takes care of edge cases of text when only 1 bottle is present 'bottle' is not pluralized.

--- bottle_song_lyrics.rb
+++ bottle_song_lyrics.rb
@@ -2,5 +2,22 @@
 # Returns the lyrics to "n Bottles of Beer".
 
 def bottle_song_lyrics(n)
-  "The lyrics to #{n} Bottles of Beer"
+  bottles = n
+  lyrics = ""
+
+  while bottles > 2
+    lyrics +=
+      "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.\nTake one down, pass it around, #{bottles - 1} bottles of beer on the wall!\n\n"
+    bottles -= 1
+  end
+
+  while bottles > 1
+    lyrics +=
+      "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.\nTake one down, pass it around, #{bottles - 1} bottle of beer on the wall!\n\n"
+    bottles -= 1
+  end
+
+  lyrics +=
+    "#{bottles} bottle of beer on the wall, #{bottles} bottle of beer.\nTake one down, pass it around, #{bottles - 1} bottles of beer on the wall!\n\n"
+
 end
